bbbbbf123875518fcc2b535a834a827b11b37e63
Salvador Ortiz <sog@msg.mx>
2023-02-10 15:27:58 -0600

    Clean unused (yet) code

--- src/email.h
+++ src/email.h
@@ -35,25 +35,24 @@
 
  **************************************************************************************************/
 
+#ifdef EMAIL_ENABLED
 //                                          S e t t i n g s
 
-  const int EmailAttemptTime = 30;                          // how often to re-attempt failed email sends (seconds)
-
-  const int MaxEmailAttempts = 5;                           // maximum email send attempts
-
+const int EmailAttemptTime = 30;                          // how often to re-attempt failed email sends (seconds)
+const int MaxEmailAttempts = 5;                           // maximum email send attempts
 
 // blank settings
-  #define _emailReceiver "<email address>"                  // address to send emails to
-  #define _smsReceiver "<email address>"                    // address to send text messages to
-  #define _UserDomain "<domain to report from>"             // user domain to report in email
-  #define _mailUser "<email address>"                       // address to send from
-  #define _mailPassword "<email password>"                  // email password
-  #define _SMTP "<smtp server>"                             // smtp server address
-  #define _SMTP_Port 587                                    // port to use (gmail: Port for SSL: 465, Port for TLS/STARTTLS: 587)
+#define _emailReceiver "<email address>"                  // address to send emails to
+#define _smsReceiver "<email address>"                    // address to send text messages to
+#define _UserDomain "<domain to report from>"             // user domain to report in email
+#define _mailUser "<email address>"                       // address to send from
+#define _mailPassword "<email password>"                  // email password
+#define _SMTP "<smtp server>"                             // smtp server address
+#define _SMTP_Port 587                                    // port to use (gmail: Port for SSL: 465, Port for TLS/STARTTLS: 587)
 
 
-const int maxMessageLength = 500;                             // maximum length of email message
-const int maxSubjectLength = 150;                             // maximum length of email subject
+const int maxMessageLength = 500;                         // maximum length of email message
+const int maxSubjectLength = 150;                         // maximum length of email subject
 
 
 //  ----------------------------------------------------------------------------------------
@@ -67,33 +66,31 @@ int emailAttemptCounter = 0;                                // counter for faile
 #include <ESP_Mail_Client.h>
 
 // stores for email messages
-  char _message[maxMessageLength];
-  char _subject[maxSubjectLength];
-  char _recepient[80];
+char _message[maxMessageLength];
+char _subject[maxSubjectLength];
+char _recepient[80];
 
 // forward declarations
-    void smtpCallback(SMTP_Status status);
-    bool sendEmail(char*, char* , char*);
-    void EMAILloop();
+void smtpCallback(SMTP_Status status);
+bool sendEmail(char*, char* , char*);
+void EMAILloop();
 
 /* The SMTP Session object used for Email sending */
-  SMTPSession smtp;
+SMTPSession smtp;
 
 
 // ----------------------------------------------------------------------------------------
 
 
 // Called from LOOP to handle emails
-
 void EMAILloop() {
+    if (!emailToSend || emailAttemptCounter >= MaxEmailAttempts) return;
 
-  if (!emailToSend || emailAttemptCounter >= MaxEmailAttempts) return;
+    if (lastEmailAttempt > 0 && (unsigned long)(millis() - lastEmailAttempt) < (EmailAttemptTime * 1000)) return;
 
-  if (lastEmailAttempt > 0 && (unsigned long)(millis() - lastEmailAttempt) < (EmailAttemptTime * 1000)) return;
-
-  // try to send the email
+    // try to send the email
     if (sendEmail(_recepient, _subject, _message)) {
-      // email sent ok
+        // email sent ok
         emailToSend = 0;                                  // clear flag that there is an email waiting to be sent
         sendSMSflag = 0;                                  // clear sms flag
         _recepient[0]=0; _message[0]=0; _subject[0]=0;    // clear all stored email text
@@ -109,7 +106,6 @@ void EMAILloop() {
           sendSMSflag = 0;                                // clear sms flag
         }
     }
-
 }  // EMAILloop
 
 
@@ -121,29 +117,29 @@ void EMAILloop() {
 
 bool sendEmail(char* emailTo, char* emailSubject, char* emailBody) {
 
-  if (serialDebug) Serial.println("----- sending an email -------");
+    if (serialDebug) Serial.println("----- sending an email -------");
 
-  // enable debug info on serial port
+    // enable debug info on serial port
     if (serialDebug) {
-      smtp.debug(1);                       // turn debug reporting on
-      smtp.callback(smtpCallback);         // Set the callback function to get the sending results
+        smtp.debug(1);                       // turn debug reporting on
+        smtp.callback(smtpCallback);         // Set the callback function to get the sending results
     }
 
-  // Define the session config data which used to store the TCP session configuration
+    // Define the session config data which used to store the TCP session configuration
     ESP_Mail_Session session;
 
-  // Set the session config
+    // Set the session config
     session.server.host_name =  _SMTP;
     session.server.port = _SMTP_Port;
     session.login.email = _mailUser;
     session.login.password = _mailPassword;
     session.login.user_domain = _UserDomain;
 
-  // Define the SMTP_Message class variable to handle to message being transported
+    // Define the SMTP_Message class variable to handle to message being transported
     SMTP_Message message;
     // message.clear();
 
-  // Set the message headers
+    // Set the message headers
     message.sender.name = _SenderName;
     message.sender.email = _mailUser;
     message.subject = emailSubject;
@@ -152,73 +148,65 @@ bool sendEmail(char* emailTo, char* emailSubject, char* emailBody) {
     // message.addCc("email3");
     // message.addBcc("email4");
 
-  // Set the message content
+    // Set the message content
     message.text.content = emailBody;
 
-  // Misc settings
+    // Misc settings
     message.text.charSet = "us-ascii";
     message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
     message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_high;
     message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
     // message.addHeader("Message-ID: <abcde.fghij@gmail.com>");    // custom message header
 
-//  // Add attachment to the message
-//    message.addAttachment(att);
+    // Add attachment to the message
+    //message.addAttachment(att);
 
-  // Connect to server with the session config
+    // Connect to server with the session config
     if (!smtp.connect(&session)) {
-      log_system_message("Sending email '" + String(_subject) +"' failed, SMTP: " + smtp.errorReason());
-      return 0;
+        log_system_message("Sending email '" + String(_subject) +"' failed, SMTP: " + smtp.errorReason());
+        return 0;
     }
 
-  // Start sending Email and close the session
+    // Start sending Email and close the session
     if (!MailClient.sendMail(&smtp, &message, true)) {
-      log_system_message("Sending email '" + String(_subject) +"' failed, Send: " + smtp.errorReason());
-      return 0;
+        log_system_message("Sending email '" + String(_subject) +"' failed, Send: " + smtp.errorReason());
+        return 0;
     } else {
-      log_system_message("Email '" + String(_subject) +"' sent ok");
-      return 1;
+        log_system_message("Email '" + String(_subject) +"' sent ok");
+        return 1;
     }
-
 }
 
 
 // ----------------------------------------------------------------------------------------
 
-
 /* Callback function to get the Email sending status */
 void smtpCallback(SMTP_Status status) {
+    if (!serialDebug) return;
 
-  if (!serialDebug) return;
+    /* Print the current status */
+    Serial.println(status.info());
 
-  /* Print the current status */
-  Serial.println(status.info());
-
-  /* Print the sending result */
-  if (status.success())
-  {
-    Serial.println("----------------");
-    Serial.printf("Message sent success: %d\n", status.completedCount());
-    Serial.printf("Message sent failled: %d\n", status.failedCount());
-    Serial.println("----------------\n");
-    struct tm dt;
-
-    for (size_t i = 0; i < smtp.sendingResult.size(); i++)
-    {
-      /* Get the result item */
-      SMTP_Result result = smtp.sendingResult.getItem(i);
-      time_t ts = (time_t)result.timestamp;
-      localtime_r(&ts, &dt);
-
-      Serial.printf("Message No: %d\n", i + 1);
-      Serial.printf("Status: %s\n", result.completed ? "success" : "failed");
-      Serial.printf("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
-      Serial.printf("Recipient: %s\n", result.recipients);
-      Serial.printf("Subject: %s\n", result.subject);
+    /* Print the sending result */
+    if (status.success()) {
+        Serial.println("----------------");
+        Serial.printf("Message sent success: %d\n", status.completedCount());
+        Serial.printf("Message sent failled: %d\n", status.failedCount());
+        Serial.println("----------------\n");
+        struct tm dt;
+        for (size_t i = 0; i < smtp.sendingResult.size(); i++) {
+            /* Get the result item */
+            SMTP_Result result = smtp.sendingResult.getItem(i);
+            time_t ts = (time_t)result.timestamp;
+            localtime_r(&ts, &dt);
+            Serial.printf("Message No: %d\n", i + 1);
+            Serial.printf("Status: %s\n", result.completed ? "success" : "failed");
+            Serial.printf("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+            Serial.printf("Recipient: %s\n", result.recipients);
+            Serial.printf("Subject: %s\n", result.subject);
+        }
+        Serial.println("----------------\n");
     }
-    Serial.println("----------------\n");
-  }
-
 }
-
+#endif
 // --------------------------- E N D -----------------------------
--- src/ftp.h
+++ src/ftp.h
@@ -8,9 +8,8 @@
 
 // Note: this library does not provide any feedback so the sketch does not know if the FTP actually worked ok or not
 
-#include <Arduino.h>    // required by PlatformIO
+#ifdef FTP_ENABLED
 #include <ESP32_FTPClient.h>
-
 
 // **************************************** S e t t i n g s ****************************************
 
@@ -26,7 +25,7 @@ char ftp_pass[]   = "<ftp password>";
 ESP32_FTPClient ftp (ftp_server,ftp_user,ftp_pass, 5000, 2);             // timeout and debbug mode on the last 2 arguments
 
 // forward declarations
-  void uploadImageByFTP(uint8_t*, size_t, String);
+void uploadImageByFTP(uint8_t*, size_t, String);
 
 
 // ----------------------------------------------------------------
@@ -35,16 +34,11 @@ ESP32_FTPClient ftp (ftp_server,ftp_user,ftp_pass, 5000, 2);             // time
 // pass image frame buffer pointer, length, file name to use
 
 void uploadImageByFTP(uint8_t* buf, size_t len, String fName) {
+    if (serialDebug) Serial.println("FTP image");
 
-  if (serialDebug) Serial.println("FTP image");
+    ftp.OpenConnection();
 
-  ftp.OpenConnection();
-
-  // // Make a new directory
-  //  ftp.InitFile("Type A");
-  //  ftp.MakeDir("my_new_dir");
-
-  //Create a file and write the image data to it;
+    //Create a file and write the image data to it;
     ftp.ChangeWorkDir("/");
     ftp.InitFile("Type I");
     fName += ".jpg";
@@ -52,10 +46,8 @@ void uploadImageByFTP(uint8_t* buf, size_t len, String fName) {
     ftp.WriteData(buf, len);
     ftp.CloseFile();
 
-  ftp.CloseConnection();
-
-  // log_system_message("FTP");
+    ftp.CloseConnection();
 }
 
-
+#endif
 // ---------------------------------------------- end ----------------------------------------------
